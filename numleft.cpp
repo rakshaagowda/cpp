5d8d61e452547a9c0ebf1dcba99e586e2b4c3e21
RAKSHA B R <rakshagowdah264@gmail.com>
2024-09-09 13:26:35 +0530

    add new code

---
+++ numleft.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter n";
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        int j;
+        for( j=1;j<=i;j++)
+        {
+            cout<<i;
+            
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+
+}
